7d00815db7728d7805bbe008bda40586f31823c1
Dani Setien <dani.setien@gmail.com>
2023-03-16 09:35:37 +0100

    Fix import extension

--- src/server/middlewares/ping/ping.ts
+++ src/server/middlewares/ping/ping.ts
@@ -1,5 +1,5 @@
 import { type NextFunction, type Request, type Response } from "express";
-import responses from "../../../utils/responses";
+import responses from "../../../utils/responses.js";
 
 const ping = (req: Request, res: Response, next: NextFunction) => {
   res.status(responses.success.statusCode).json({ ping: "pong 🏓" });
